b6e1be5463d0abf45ae69e0798184d8e8998ce56
2439905184 <suchenglong018@126.com>
2022-01-31 19:54:15 +0800

    初步完成用户界面设计

--- js/user.js
+++ js/user.js
@@ -1,5 +1,114 @@
+
 $(function()
 {
+    //编辑信息
+    $("#edit_btn").click(function()
+    {
+        //todo 重构到单独的html里面去
+        $("#profile_edit #content").empty()
+        $("#profile_edit #content").append('<input id="file" type="file" style="display:none">')
+        $("#profile_edit #content").append("<label>昵称</label>")
+        $("#profile_edit #content").append("<input type='text' placeholder='请输入昵称'/>")
+        $("#profile_edit #content").append("<br/>")
+
+        $("#profile_edit #content").append("<span>用户头像</span><img id='edit_head' src='img/sand.png' width='100px' />")
+        $("#profile_edit #content").append("<br/>")
+
+        $("#profile_edit #content").append("<label>性别</label>")
+        $("#profile_edit #content").append("<form action='.php'> <input type='radio' name='sex'/>男<input type='radio'name='sex'/> 女</form>")
+        $("#profile_edit #content").append("<br/>")
+
+        $("#profile_edit #content").append("<span>所在地</span><input placeholder='请输入所在地'>")
+        $("#profile_edit #content").append("<br/>")
+
+        $("#profile_edit #content").append("<span>个人介绍</span><textarea placeholder='请输入你的个人介绍'></textarea>")
+        $("#profile_edit #content").append("<br/>")
+
+        $("#profile_edit #content").append("<span>qq</span><input placeholder='请输入你的qq'>")
+        $("#profile_edit #content").append("<br/>")
+
+        $("#profile_edit #content").append("<span>微信</span><input placeholder='请输入你的微信'>")
+
+        $("#profile_edit #content").append("<br/>")
+        $("#profile_edit #content").append("<button id='save'>保存</button>")
+        $("#profile_edit #content").append("<button id='back'>返回</button>")
+        $("#back").on("click",function()
+        {
+            //alert("返回！")
+            $("#profile_edit #content").empty()
+            $.get("instance/user_content.html",function(data,status)
+            {
+                console.log(data)
+                $("#profile_edit").append(data)
+            })
+            
+        })
+        $("#save").on("click",function()
+        {
+            alert("信息已保存！")
+        })
+        $("#edit_head").click(function()
+        {
+
+        })
+        //更换头像
+        $("#edit_head").click(function()
+        {
+            //console.log("更换头像！")
+            $("#file").trigger("click")
+        })
+        $("#file").on("change",function()
+        {
+            console.log(this)
+            var file = event.target.files[0]
+            console.log(file)
+            var pettern = /^image/;                
+            console.info(file.type)
+            if (!pettern.test(file.type)) 
+            {
+                alert("图片格式不正确");
+                return;
+            }
+            var fr = new FileReader()
+            fr.readAsDataURL(file)
+            fr.onload = function(e)
+            {
+                console.log(e)
+                var result = this.result
+                console.log(result)
+                $("#edit_head").attr("src",result)
+            }
+            //var final = fr.readAsArrayBuffer(file)
+            //console.log(final)
+        })
+    })
+    
+    $("#profile_root li").click(function()
+    {
+        var lit = $(this).text()
+        $(this).css("border-left","4px solid #1890ff").siblings().css("border-left","0px")
+        $(this).css("background-color","rgba(53, 51, 51, 0.244)").siblings().css("background-color","#FFFFFF")
+        if(lit == "基本信息")
+        {
+            $("#profile_edit #content").empty()
+            $("#profile_edit #title b").text("基本信息")
+            $.get("instance/user_content.html",function(data,status)
+            {
+                $("#profile_edit #content").append(data)
+            })
+            
+        }
+        if(lit == "账号绑定")
+        {
+            $("#profile_edit #content").empty()
+            $("#profile_edit #title b").text("账号绑定")
+            $.get("instance/user_account_bind.html",function(data,status)
+            {
+                $("#profile_edit #content").append(data)
+            })
+        }
+
+    })
     $("#id").click(function()
     {
 
@@ -52,3 +161,7 @@ $(function()
         $("#user_skill_tag").empty()
     })
 })
+/*function get_file_path()
+    {
+        alert("#file").attr("value")
+    }*/
