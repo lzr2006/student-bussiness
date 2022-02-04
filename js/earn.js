ad3ba9f67048fcbd3e3bb57f12b793c1470c6c44
2439905184 <suchenglong018@126.com>
2022-02-04 16:00:50 +0800

    将jquery屎山代码用模板渲染技术重构！

--- js/earn.js
+++ js/earn.js
@@ -4,6 +4,11 @@ $(function()
     {
         location.reload()
     })
+    //todo 更新模板数据
+    function update_model()
+    {
+
+    }
     //改天研究下react jq太烦了
     $.get("api/get_checked_tasks.php",function(data,status)
     {
@@ -12,51 +17,23 @@ $(function()
             var json = JSON.parse(data)
             console.log(json)
             var tmpl = $.templates("#myTmpl")
-            var data = 
-            {
-                'name'    : json[0].title,
-                'price' : 100,
-            }
-            var html = tmpl.render(data)
-            for(i in json)
-            {
-                $("#list").append(html)
-            }
             //预处理json 按每10个元素分割->array_utils.js
-            /*var jsonUtil = new JsonUtil()
+            var jsonUtil = new JsonUtil()
             var tmp = jsonUtil.toArray(data)
             var result = jsonUtil.arraySplit(tmp,10)
             console.log("分割数组！")
             console.log(result)
-            $("#list").append("A")
-            $("#list").append("<p>"+result[0][0]+"</p>")*/
-            /*for(item in result)
+            //第一页
+            for(i in result[0])
             {
-               
-            }*/
-            /*for(var i=0;i<json.length;i++)
-            {
-                var content_wrapper = $("<div class='li'></div>")
-                var content_img = $("<img/>")
-                var content_a = $("<a href='task_detail.html' target='_blank'></a>")
-                var content_span = $("<span class='proj_state'>项目状态</span>")
-                var content_span_price = $("<span class='price'>价格</span>")
-                
-                $(content_img).attr("src","img/sand.png")
-                $(content_img).attr("width","100px")
-                $(content_img).attr("alt","项目图片预览")
-                $(content_a).text(json[i].title)
-                $(content_span_price).text("价格"+ json[i].price)
-                
-                $(content_wrapper).append(content_img)
-                $(content_wrapper).append(content_a)
-                $(content_wrapper).append(content_span)
-                $(content_wrapper).append("<span>招标中</span>")
-                $(content_wrapper).append(content_span_price)
-                $(content_wrapper).append("<p>技能要求:Java</p>")
-
-                $("#list").append(content_wrapper)
-            }*/
+                var tmpl_data = 
+                {
+                    'title'    : json[i].title,
+                    'price' : json[i].price,
+                }
+                var html = tmpl.render(tmpl_data)
+                $("#list").append(html)
+            }
         }
         else
         {
